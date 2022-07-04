0964a261aff41102b042d9e4f134ea83afd20f2e
WHL99 <xjv298347@gmail.com>
2022-07-04 13:37:34 +0200

    fix User model Schema

--- models/User.js
+++ models/User.js
@@ -1,26 +1,41 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    user: {
-      type: String,
-      unique: true,
-      required: true,
-      firstName: String,
-      lastName: String,
-      birthday: Date,
-      gender: { type: String, enum: ["male", "female", "other"] },
-      email: String,
-      phoneNumber: Number,
-    },
-    password: String,
-  },
-  {
-    timestamps: true,
-  }
-);
+// const userSchema = new Schema(
+//   {
+//     username: {
+//       type: String,
+//       unique: true,
+//       required: true,
+//       firstName: String,
+//       lastName: String,
+//       birthday: Date,
+//       sex: { type: String, enum: ['male', 'female', 'other'] },
+//       email: String,
+//       phoneNumber: Number,
+//     },
+//     password: String,
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  firstName: String,
+  lastName: String,
+  birthday: { type: Date },
+  gender: { type: String, enum: ['male', 'female', 'other'] },
+  phoneNumber: Number,
+  password: String,
+});
+
+
 
 const User = model("User", userSchema);
 
 module.exports = User;
+
+
+
