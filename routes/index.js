d29f1e9b90540ac2da83f1cd4fef8a7d7d368fb5
WHL99 <xjv298347@gmail.com>
2022-07-07 11:07:53 +0200

    create select-rooms-by-city route and view

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const User = require("../models/User");
-
 const router = require("express").Router();
-
+const Room = require("../models/Room");
 const fileUploader = require('../config/cloudinary.config');
 
 /* GET home page */
@@ -14,13 +13,13 @@ router.get("/add-profile", (req, res, next) => {
   res.render("add-profile");
 });
 
-router.post("/add-profile",fileUploader.single('profile-image'), (req, res, next) => {
+router.post("/add-profile", fileUploader.single('profile-image'), (req, res, next) => {
   const { gender, phoneNumber, city, birthday } = req.body;
   const imageUrl = req.file.path
   console.log(req.body)
   //console.log(req.session.currentUser)
   const userId = req.session.currentUser._id
-  User.findByIdAndUpdate(userId,{ gender, phoneNumber, city, birthday,imageUrl })
+  User.findByIdAndUpdate(userId, { gender, phoneNumber, city, birthday, imageUrl })
     .then(() => {
       res.redirect("/profile");
     })
@@ -31,27 +30,28 @@ router.post("/add-profile",fileUploader.single('profile-image'), (req, res, next
 
 router.get('/profile', (req, res, next) => {
   const userId = req.session.currentUser._id
-	User.findById(userId)
-		.then(usersFromDB => {
-			res.render('profile', { oneUserData: usersFromDB })
-		})
-		.catch(err => {
-			next(err)
-		})
+  User.findById(userId)
+    .then(usersFromDB => {
+      res.render('profile', { oneUserData: usersFromDB })
+    })
+    .catch(err => {
+      next(err)
+    })
 })
 
 
 //search rooms by city
 router.get("/room-search", (req, res, next) => {
-  Room.find(req.query.searchByCity)
-    .then((roomsFromDB) => {
-      res.render("room-search-results", {seeRoomsByCity: roomsFromDB})
+  Room.find({ city: req.query.searchByCity })
+    .then(roomsFromDB => {
+      res.render("rooms/select-rooms-by-city", { seeRoomsByCity: roomsFromDB })
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
 });
-// {city:req.query.q}
 
 module.exports = router;
+
+
 
