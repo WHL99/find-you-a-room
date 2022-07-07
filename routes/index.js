049a385ab6544feaa5024dc61a8747678db2ab92
alexmovact <alexander@movact.de>
2022-07-07 11:46:34 +0200

    styling

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 
 const router = require("express").Router();
 
-const fileUploader = require('../config/cloudinary.config');
+const fileUploader = require("../config/cloudinary.config");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -14,31 +14,40 @@ router.get("/add-profile", (req, res, next) => {
   res.render("add-profile");
 });
 
-router.post("/add-profile",fileUploader.single('profile-image'), (req, res, next) => {
-  const { gender, phoneNumber, city, birthday } = req.body;
-  const imageUrl = req.file.path
-  console.log(req.body)
-  //console.log(req.session.currentUser)
-  const userId = req.session.currentUser._id
-  User.findByIdAndUpdate(userId,{ gender, phoneNumber, city, birthday,imageUrl })
-    .then(() => {
-      res.redirect("/profile");
+router.post(
+  "/add-profile",
+  fileUploader.single("profile-image"),
+  (req, res, next) => {
+    const { gender, phoneNumber, city, birthday } = req.body;
+    const imageUrl = req.file.path;
+    console.log(req.body);
+    //console.log(req.session.currentUser)
+    const userId = req.session.currentUser._id;
+    User.findByIdAndUpdate(userId, {
+      gender,
+      phoneNumber,
+      city,
+      birthday,
+      imageUrl,
+    })
+      .then(() => {
+        res.redirect("/profile");
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+);
+
+router.get("/profile", (req, res, next) => {
+  const userId = req.session.currentUser._id;
+  User.findById(userId)
+    .then((usersFromDB) => {
+      res.render("profile", { oneUserData: usersFromDB });
     })
     .catch((err) => {
       next(err);
     });
 });
 
-router.get('/profile', (req, res, next) => {
-  const userId = req.session.currentUser._id
-	User.findById(userId)
-		.then(usersFromDB => {
-			res.render('profile', { oneUserData: usersFromDB })
-		})
-		.catch(err => {
-			next(err)
-		})
-})
-
 module.exports = router;
-
